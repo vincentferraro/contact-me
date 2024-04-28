b18d1105dfa7e9efb814210b7b0d8268210aa8e1
vincentferraro <v.ferraro@live.fr>
2024-04-28 10:04:58 -0400

    form handling validation schema

--- client/src/components/FormUser.js
+++ client/src/components/FormUser.js
@@ -1,47 +1,58 @@
-import { Formik, Form, Field } from 'formik'
+import { Formik, Form } from 'formik'
 import { TextField } from '@mui/material';
 import { Button } from '@mui/material';
+import * as yup from 'yup'
+
+const validationSchema = yup.object({
+    username: yup.string('Enter your username').required('username is required'),
+    email: yup.string('Enter your email').required('email is required'),
+    message: yup.string('Enter your message').required('message is required'),
+})
+
+
 
 function FormUser(){
 
+    
     return(
         <div className='flex justify-center w-3/6 h-4/6 '>
             <Formik
             initialValues={{
-                userName:'',
+                username:'',
                 email:'',
                 message:''}}
-                validate={values =>{
+            validationSchema={validationSchema}
+            validate={values =>{
                     const errors = {};
-                    if(!values.email | !values.userName){
+                    if(!values.email | !values.username){
                         errors.email = 'Required';
                     }else if(!/^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,}$/i.test(values.email)){
                         errors.email = 'Invalid email address';
                     }
+                    console.log(errors)
                     return errors;
                 }}
-                onSubmit={(values, { setSubmitting }) => {
-                    setTimeout(() => {
-                      alert(JSON.stringify(values, null, 2));
-                      setSubmitting(false);
-                    }, 400);
+            onSubmit={(values) => {
+               console.log('Submitting...',values)
                   }}>
-                    {({
-                        values,
-                        errors,
-                        touched,
-                        handleChange,
-                        handleBlur
-                    })=>(
-                        <Form  className="flex flex-col h-[100%] w-5/6 justify-center items-center ">
+                    {(props)=>(
                         
-                        <TextField id="userName" className="inline-block m-6 p-6 " name="userName" label="Username" margin="normal" error helperText="Incorrect entry." onChange={handleChange}
-                                onBlur={handleBlur}
-                                value={values.email}/>
+                        <Form  className="flex flex-col h-[100%] w-5/6 justify-center items-center " onSubmit={props.handleSubmit}>
+                        
+                        <TextField id="username" className="inline-block m-6 p-6 " name="username" label="username" margin="normal" 
+                                onChange={props.handleChange}
+                                onBlur={props.handleBlur}
+                                value={props.values.username}/>
   
-                        <TextField  id="email" className="block m-6" name="email" label="Email"  error helperText="Incorrect entry." type="email" margin="normal"/>   
+                        <TextField  id="email" className="block m-6" name="email" label="Email"   type="email" margin="normal" 
+                                onChange={props.handleChange}
+                                onBlur={props.handleBlur}
+                                value={props.values.email}/>   
              
-                        <TextField id="message" className="block m-6" name="message" label="Message" error helperText="Incorrect entry." multiline fullWidth rows={6} minRows={4} maxRows={6} margin="normal" />
+                        <TextField id="message" className="block m-6" name="message" label="Message" multiline fullWidth minRows={4} maxRows={6} margin="normal"
+                                onChange={props.handleChange}
+                                onBlur={props.handleBlur}
+                                value={props.values.message} />
 
                         <Button className="bg-slate-500" type="submit" variant="contained">Submit</Button>
 

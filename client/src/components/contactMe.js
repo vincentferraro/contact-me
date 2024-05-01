1cf970341265f6d6d4e7686fa052cd21a89944fc
vincentferraro <v.ferraro@live.fr>
2024-04-30 21:15:35 -0400

    adding style in contactMe component

--- client/src/components/contactMe.js
+++ client/src/components/contactMe.js
@@ -5,16 +5,20 @@ function ContactMe(){
 
     return(
     
-    <div className="w-3/6 h-4/6 border-solid border-2 flex flex-col justify-center items-start">
-        <div className="flex flex-row items-baseline m-11">
-        <Avatar  alt="Vincent Ferraro" src="" sx={{width:40, height:40,bgcolor: "red"}}>VF</Avatar>
-        <Typography className="text-2xl m-5">Hi, I'm Vincent 👋</Typography>
+    <div className="w-3/6 h-4/6 flex flex-col justify-center items-start">
+        <div className=" flex flex-row justify-center w-[100%]">
+
+        <Avatar alt="Vincent Ferraro" src="" sx={{width:110, height:110,bgcolor: "red"}}>VF</Avatar>  
+        </div>
+        <div className="flex flex-row items-baseline m-11 mt-5 ">
+                
+        <Typography className="text-2xl" fontSize={30}>Hi, I'm Vincent 👋</Typography>
         </div>
 
-        <div className="border-solid border-red-400 border-2 m-11 ">
-            <Typography>
-                I'm full stack developper NodeJs, I can create amazing app with React and NodeJs, I use PostgresSQL.
-                Don't hesitate to contact me for some question about my skills
+        <div className="border-solid  m-11">
+            <Typography fontSize={30} >
+                Full stack developper <b>NodeJs</b>, I can create amazing app with <b>React</b>, <b>PostgresSQL</b> and <b>MongoDB</b> .
+    
             </Typography>
         </div>
         
